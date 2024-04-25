c018ea5e3fb54086a78c38da56c67004ea22da2b
bangbang93 <bangbang93@163.com>
2024-04-25 12:36:32 +0800

    Add unique-in-order solution in Rust

---
+++ codewars/rust/unique-in-order-54e6533c92449cc251001667/src/main.rs
@@ -0,0 +1,33 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+fn unique_in_order<T>(sequence: T) -> Vec<T::Item>
+where
+    T: std::iter::IntoIterator,
+    T::Item: std::cmp::PartialEq + std::fmt::Debug,
+{
+    let mut result = Vec::new();
+    for item in sequence {
+        if result.last() != Some(&item) {
+            result.push(item);
+        }
+    }
+    result
+}
+
+// Add your tests here.
+// See https://doc.rust-lang.org/stable/rust-by-example/testing/unit_testing.html
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample_test() {
+        assert_eq!(
+            unique_in_order("AAAABBBCCDAABBB".chars()),
+            vec!['A', 'B', 'C', 'D', 'A', 'B']
+        );
+    }
+}
